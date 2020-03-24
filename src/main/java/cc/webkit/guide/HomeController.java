b003d94afd6613e56ff7bed81f093c4ea68c9551
wuhuaji0 <wuhuaji0@gmail.com>
2020-03-24 16:24:50 +0800

    init .

---
+++ src/main/java/cc/webkit/guide/HomeController.java
@@ -0,0 +1,14 @@
+package cc.webkit.guide;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String hello(Model model){
+        model.addAttribute("msg","nihao");
+        return "index";
+    }
+}
---
+++ src/main/java/cc/webkit/guide/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cc.webkit.guide;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GuideApplication.class);
+	}
+
+}
