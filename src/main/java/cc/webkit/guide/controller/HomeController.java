facde0636d886b53927239c4b79ca7e53dd39f35
wuhuaji0 <wuhuaji0@gmail.com>
2020-03-31 09:39:38 +0800

    首页基本样式

--- src/main/java/cc/webkit/guide/controller/HomeController.java
+++ src/main/java/cc/webkit/guide/controller/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,5 +29,18 @@ public class HomeController {
         }
         model.addAttribute("categories",result);
         return "index";
+    }
+
+
+    @ResponseBody
+    @GetMapping("/test")
+    public List<Map<String,Object>> test(){
+        List<Map<String, Object>> categories = jdbcTemplate.queryForList("select * from category");
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (Map<String,Object> category : categories){
+            category.put("urls",jdbcTemplate.queryForList("select * from url where category_id = " + category.get("id")));
+            result.add(category);
+        }
+        return result;
     }
 }
