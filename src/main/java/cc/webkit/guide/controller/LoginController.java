50d7c2a3e32b0da2d87c9d1d495850a73454f765
wuhuaji0 <wuhuaji0@gmail.com>
2020-03-31 21:45:53 +0800

    用户注册

--- src/main/java/cc/webkit/guide/controller/LoginController.java
+++ src/main/java/cc/webkit/guide/controller/LoginController.java
@@ -3,6 +3,8 @@ package cc.webkit.guide.controller;
 import cc.webkit.guide.model.Resp;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,9 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping("/login")
     public String login(){
@@ -29,9 +34,35 @@ public class LoginController {
         return mapper.writeValueAsString(new Resp(0,"登录成功"));
     }
 
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+
+    @ResponseBody
+    @PostMapping("/doRegister")
+    public String doRegister(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletRequest request, HttpSession session) throws JsonProcessingException {
+
+        // todo: 密码要加密
+        String sql = "INSERT INTO `admin` (`username`, `password`) VALUES (?, ?);";
+        int result = jdbcTemplate.update(sql,username,password);
+        ObjectMapper mapper = new ObjectMapper();
+
+        // todo: 这里要返回 conte-type: json，方便前端使用
+        Resp resp = result > 0 ? new Resp(0,"注册成功") : new Resp(-1,"注册失败");
+        if (result > 0){
+            session.setAttribute("user",username);
+        }
+        return mapper.writeValueAsString(resp);
+    }
+
     @GetMapping("/logout")
     public String logout(HttpSession session) throws JsonProcessingException {
         session.removeAttribute("user");
         return "redirect:/";
     }
+
+
 }
