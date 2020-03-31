093493eeb6a5287b26c5be8996f36c5b188a676c
wuhuaji0 <wuhuaji0@gmail.com>
2020-03-31 09:10:02 +0800

    基本页面布局

--- src/main/java/cc/webkit/guide/controller/HomeController.java
+++ src/main/java/cc/webkit/guide/controller/HomeController.java
@@ -2,21 +2,29 @@ package cc.webkit.guide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/")
-    public List<Map<String, Object>> hello(Model model){
-        return jdbcTemplate.queryForList("select * from category");
+    public String hello(Model model){
+        List<Map<String, Object>> categories = jdbcTemplate.queryForList("select * from category");
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (Map<String,Object> category : categories){
+            category.put("urls",jdbcTemplate.queryForList("select * from url where category_id = " + category.get("id")));
+            result.add(category);
+        }
+        model.addAttribute("categories",result);
+        return "index";
     }
 }
