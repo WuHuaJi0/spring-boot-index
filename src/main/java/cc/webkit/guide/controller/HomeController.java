6351839a4815536b193ac849f142213f5e6843c7
wuhuaji0 <wuhuaji0@gmail.com>
2020-03-31 17:57:09 +0800

    判断是否已登录，在 nav 中显示不同的内容

--- src/main/java/cc/webkit/guide/controller/HomeController.java
+++ src/main/java/cc/webkit/guide/controller/HomeController.java
@@ -4,11 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,7 +18,7 @@ public class HomeController {
     JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/")
-    public String hello(Model model){
+    public String hello(Model model, HttpServletRequest request){
         List<Map<String, Object>> categories = jdbcTemplate.queryForList("select * from category");
         List<Map<String, Object>> result = new ArrayList<>();
         for (Map<String,Object> category : categories){
@@ -31,16 +29,4 @@ public class HomeController {
         return "index";
     }
 
-
-    @ResponseBody
-    @GetMapping("/test")
-    public List<Map<String,Object>> test(){
-        List<Map<String, Object>> categories = jdbcTemplate.queryForList("select * from category");
-        List<Map<String, Object>> result = new ArrayList<>();
-        for (Map<String,Object> category : categories){
-            category.put("urls",jdbcTemplate.queryForList("select * from url where category_id = " + category.get("id")));
-            result.add(category);
-        }
-        return result;
-    }
 }
